6ab57efa7c7dc846bcd65415501fd0a9ad09cf1b
Carlos Duarte Rodríguez <130692626+CarlosDuarteRod@users.noreply.github.com>
2023-06-14 11:02:50 +0000

    proyecto medio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,48 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function combiArrays(array1, array2, array3) {
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  let string = "";
+
+  for (let i in array1) {
+    let primerArray = array1[i];
+
+    for (let j in array2) {
+      let segundoArray = array2[j];
+
+      for (let x in array3) {
+        let tercerArray = array3[x];
+        string += pronoun(primerArray) + adj(segundoArray) + noun(tercerArray);
+      }
+    }
+  }
+  document.getElementById("dominio").innerHTML = string;
 };
+
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+
+function combiArrays(array1, array2, array3) {
+  let string = "";
+
+  for (let i in array1) {
+    let primerArray = array1[i];
+
+    for (let j in array2) {
+      let segundoArray = array2[j];
+
+      for (let x in array3) {
+        let tercerArray = array3[x];
+        string += primerArray + segundoArray + tercerArray;
+      }
+    }
+  }
+  return string;
+}
+
+console.log(combiArrays(pronoun, adj, noun));
