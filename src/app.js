bfd6b1029eb7614bf22c7b388eb68f77677f53b6
Carlos Duarte Rodríguez <130692626+CarlosDuarteRod@users.noreply.github.com>
2023-06-14 18:45:59 +0000

    Proyecto final

--- src/app.js
+++ src/app.js
@@ -5,48 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function combiArrays(array1, array2, array3) {
+window.onload = function() {
   var pronoun = ["the", "our"];
   var adj = ["great", "big"];
   var noun = ["jogger", "racoon"];
+  var extension = [".com", ".net"];
 
-  let string = "";
+  function combiArrays(array1, array2, array3, array4) {
+    let string = "";
 
-  for (let i in array1) {
-    let primerArray = array1[i];
+    for (let i in array1) {
+      let primerArray = array1[i];
 
-    for (let j in array2) {
-      let segundoArray = array2[j];
+      for (let j in array2) {
+        let segundoArray = array2[j];
 
-      for (let x in array3) {
-        let tercerArray = array3[x];
-        string += pronoun(primerArray) + adj(segundoArray) + noun(tercerArray);
+        for (let x in array3) {
+          let tercerArray = array3[x];
+
+          for (let k in array4) {
+            let cuartoArray = array4[k];
+
+            string += primerArray + segundoArray + tercerArray + cuartoArray;
+            string += "<br>";
+          }
+        }
       }
     }
+    return string;
   }
-  document.getElementById("dominio").innerHTML = string;
+
+  let totalstring = combiArrays(pronoun, adj, noun, extension);
+
+  let dominio1 = document.querySelector("#dominio");
+
+  dominio1.innerHTML = totalstring;
 };
-
-var pronoun = ["the", "our"];
-var adj = ["great", "big"];
-var noun = ["jogger", "racoon"];
-
-function combiArrays(array1, array2, array3) {
-  let string = "";
-
-  for (let i in array1) {
-    let primerArray = array1[i];
-
-    for (let j in array2) {
-      let segundoArray = array2[j];
-
-      for (let x in array3) {
-        let tercerArray = array3[x];
-        string += primerArray + segundoArray + tercerArray;
-      }
-    }
-  }
-  return string;
-}
-
-console.log(combiArrays(pronoun, adj, noun));
